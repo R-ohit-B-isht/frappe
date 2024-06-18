b676e56e36e97dafc97b7a8f736d93520be73f04
Ubuntu <ubuntu@ip-10-240-186-91.us-west-2.compute.internal>
2024-06-18 15:40:30 +0000

    Adjust mock implementation for air-datepicker

--- jest.setup.js
+++ jest.setup.js
@@ -21,19 +21,20 @@ jest.mock('air-datepicker', () => {
 
 // Extend jQuery prototype to include datepicker method
 $.fn.datepicker = function() {
+  const self = this;
   return {
     selectDate: jest.fn().mockImplementation((date) => {
-      this.selectedDates = [date];
+      self.selectedDates = [date];
     }),
     update: jest.fn().mockImplementation(() => {
       const hijriDate = '30/11/1445'; // Mock Hijri date for testing
-      this.selectedDates = [new Date(2024, 5, 18)]; // Mock Georgian date for testing
+      self.selectedDates = [new Date(2024, 5, 18)]; // Mock Georgian date for testing
       $('#datepicker').val(hijriDate);
     }),
     destroy: jest.fn(),
     show: jest.fn(),
     hide: jest.fn(),
     clear: jest.fn(),
-    getSelectedDates: jest.fn().mockReturnValue(this.selectedDates || []),
+    getSelectedDates: jest.fn().mockReturnValue(self.selectedDates || []),
   };
 };
