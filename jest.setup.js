875e1d5a89f1a5ac79955824facd7b7f7b630954
Ubuntu <ubuntu@ip-10-240-186-91.us-west-2.compute.internal>
2024-06-18 15:07:37 +0000

    Mock air-datepicker in Jest setup to resolve TypeError

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,16 @@
 import $ from 'jquery';
 global.$ = global.jQuery = $;
+
+// Mock air-datepicker to avoid issues in Jest environment
+jest.mock('air-datepicker', () => {
+  return {
+    __esModule: true,
+    default: jest.fn().mockImplementation(() => {
+      return {
+        selectDate: jest.fn(),
+        update: jest.fn(),
+        destroy: jest.fn(),
+      };
+    }),
+  };
+});
